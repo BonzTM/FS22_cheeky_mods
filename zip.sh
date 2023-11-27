d091e9f4cf34c98332cfab6b0a69797adffa1ee1
Josh <joshdwells@gmail.com>
2023-11-27 15:15:33 -0500

    updating gitignore and zip.sh

--- zip.sh
+++ zip.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-for i in $(find . -maxdepth 1 -type d -name "FS22*"); do zip -jr $i.zip $i; done
+
+mkdir -p ./zipped_files
+for i in $(find . -maxdepth 1 -type d -name "FS22*"); do zip -jr $i.zip $i; mv *.zip ./zipped_files/; done
