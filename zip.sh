0a4b8da16ad33b88c2a7049e1de52aecf9a12fa0
Josh <joshdwells@gmail.com>
2023-12-07 13:03:18 -0500

    update zip script

--- zip.sh
+++ zip.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p ./zipped_files
-for i in $(find . -maxdepth 1 -type d -name "FS22*"); do zip -jr $i.zip $i; mv *.zip ./zipped_files/; done
+for i in $(find . -maxdepth 1 -type d -name "FS22*"); do cd $i ; zip -r $i.zip ./* ; mv $i.zip ../zipped_files/; cd .. ;done
