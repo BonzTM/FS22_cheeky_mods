3db300526b660ae3f382412e53aef0904ffb4f3f
Josh <joshdwells@gmail.com>
2023-12-23 15:31:50 -0500

    add harvesters to pack

--- zip.sh
+++ zip.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 mkdir -p ./zipped_files
 for i in $(find . -maxdepth 1 -type d -name "FS22*"); do cd $i ; zip -r $i.zip ./* ; mv $i.zip ../zipped_files/; cd .. ;done
