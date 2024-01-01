fc3dce854864a80804e0dc71ea4a8f2680e6480b
Josh <joshdwells@gmail.com>
2024-01-01 17:24:06 -0500

    lots of mods merged in; mayo factory; empty pallets, etc

--- FS22_cheeky_placeables_pack/scripts/AddConstructionTab.lua
+++ FS22_cheeky_placeables_pack/scripts/AddConstructionTab.lua
@@ -11,11 +11,11 @@ function addConstructionCategory:addOwnCategory()
 		-- g_storeManager:addConstructionTab("Cheeky", "factoriesPlat", "Platinum Factories", "$dataS/menu/construction/ui_construction_icons.dds", GuiUtils.getUVs("40px 40px 32px 32px",string.getVectorN("256 256", 2)), "")
 		-- g_storeManager:addConstructionTab("Cheeky", "factoriesPrem", "Premium Factories", "$dataS/menu/construction/ui_construction_icons.dds", GuiUtils.getUVs("40px 40px 32px 32px",string.getVectorN("256 256", 2)), "")
 		g_storeManager:addConstructionTab("Cheeky", "animals", "Animals", "$dataS/menu/construction/ui_construction_icons.png", GuiUtils.getUVs("40px 120px 32px 32px",string.getVectorN("256 256", 2)), "")
-		g_storeManager:addConstructionTab("Cheeky", "sellingPoints", "Selling Points", "$dataS/menu/construction/ui_construction_icons.dds", GuiUtils.getUVs("80px 120px 32px 32px",string.getVectorN("256 256", 2)), "")
+		g_storeManager:addConstructionTab("Cheeky", "sellingPoints", "Selling Points", "$dataS/menu/construction/ui_construction_icons.dds", GuiUtils.getUVs("80px 40px 32px 32px",string.getVectorN("256 256", 2)), "")
 		g_storeManager:addConstructionTab("Cheeky", "silos", "Silos", "$dataS/menu/construction/ui_construction_icons.dds", GuiUtils.getUVs("80px 0 32px 32px",string.getVectorN("256 256", 2)), "")
 		-- g_storeManager:addConstructionTab("Cheeky", "siloExtensions", "Silo Extensions", "$dataS/menu/construction/ui_construction_icons.png", GuiUtils.getUVs("200px 0px 32px 32px",string.getVectorN("256 256", 2)), "")
         g_storeManager:addConstructionTab("Cheeky", "storages", "Storages", "$dataS/menu/construction/ui_construction_icons.dds", GuiUtils.getUVs("80px 0 32px 32px",string.getVectorN("256 256", 2)), "")
-		g_storeManager:addConstructionTab("Cheeky", "greenhouses", "Greenhouses", "$dataS/menu/construction/ui_construction_icons.dds", GuiUtils.getUVs("80px 0 32px 32px",string.getVectorN("256 256", 2)), "")
+		g_storeManager:addConstructionTab("Cheeky", "greenhouses", "Greenhouses", "$dataS/menu/construction/ui_construction_icons.dds", GuiUtils.getUVs("40px 120px 32px 32px",string.getVectorN("256 256", 2)), "")
 		-- g_storeManager:addConstructionTab("Cheeky", "containers", "Containers", "$dataS/menu/construction/ui_construction_icons.dds", GuiUtils.getUVs("120px 0 32px 32px",string.getVectorN("256 256", 2)), "")
 		
 end
