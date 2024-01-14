f82a3bf11caa6e0400140cf1dd16ca03dea3c70a
Josh <joshdwells@gmail.com>
2024-01-14 12:32:48 -0500

    fixed zip.sh encoding

--- zip.sh
+++ zip.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-mkdir -p ./zipped_files
+if [ -d './zipped_files' ]; then
+    echo 'Directory exists'
+else
+    mkdir -p ./zipped_files
+fi
+
 for i in $(find . -maxdepth 1 -type d -name "FS22*"); do cd $i ; zip -r $i.zip ./* ; mv $i.zip ../zipped_files/; cd .. ;done
